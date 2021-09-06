fc405858474fd4544a0368a018c79a1470e3ac2e
Jay Kh <45645372+arcsolace@users.noreply.github.com>
2021-09-06 17:47:12 +0700

    working database controllers!

--- database/controller/controller.js
+++ database/controller/controller.js
@@ -2,58 +2,99 @@ const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGO_URI;
 
-const client = new MongoClient(uri);
+const client = new MongoClient(uri,
+  { useUnifiedTopology: true},
+  { useNewUrlParser: true },
+  { connectTimeoutMS: 3000 },
+  { keepAlive: 1}
+);
 
 // Creates a session if the rendered room ID is a new one and not found in the database.
-exports.createSession = async function(room_id) {
+exports.createSession = async function(roomId) {
+  try {
+    if (roomId == 'favicon.ico') {
+      console.log('Favicon...');
+    } else if (roomId == 'robots.txt') {
+      console.log('Robots...');
+    } else {
+      await client.connect();
+      const database = client.db("rtctele");
+      const sessions = database.collection("sessions");
+      let operation = await sessions.findOne(
+        {
+          room: roomId
+        }, (err, result) => {
+          if (err) {
+            throw err
+          }
+          else if (result == undefined) {
+            insertSession(roomId);
+          }
+          else {
+            console.log('Room already exists');
+          }
+        }
+      );
+    };
+  } catch (err) {
+    throw err
+  }
+};
+
+// Helper function for inserting a new session
+async function insertSession(roomId) {
   try {
     await client.connect();
     const database = client.db("rtctele");
     const sessions = database.collection("sessions");
 
-    let dateNow = new Date()
-    const result = await sessions.findOne({ room: room_id })
+    let dateNow = new Date();
+    let obj = { room: roomId,
+              url: `https://rtctele.herokuapp.com/${roomId}`,
+              users: [],
+              status: 'New',
+              date: dateNow };
 
-    if (result == undefined) {
-      if (room_id == 'favicon.ico') {
-        console.log('Favicon...')
-      } else {
-        let obj = { room: room_id,
-                  url: `https://rtctele.herokuapp.com/${room_id}`,
-                  users: [],
-                  status: 'New',
-                  date: dateNow }
-        const insert = await sessions.insertOne(obj)
-        console.log(insert)
+    let operation = await sessions.insertOne(
+      obj, (err, result) => {
+        if (err) {
+          throw err
+        }
+
+        else if (result) {
+          console.log('Inserted!');
+        }
       }
-    }
+    );
   } catch (err) {
     throw err
-  } finally {
-    await client.close()
   }
-}
+};
 
 // Adds the user to the database users array.
 exports.connectUser = async function(roomId, userId) {
   try {
+    console.log("connecting user!")
     await client.connect();
     const database = client.db("rtctele");
     const sessions = database.collection("sessions");
+    let filter = { room: roomId };
+    let obj = { $push: { users: userId } };
+    let operation = await sessions.updateOne(
+      filter, obj, (err, result) => {
+        if (err) {
+          throw err
+        }
 
-    const result = await sessions.findOne({ room: roomId })
-    if (result != undefined) {
-      let filter = { room: roomId }
-      let obj = { $push: { users: userId } };
-      const update = await sessions.updateOne(filter, obj)
-      console.log(update)
-    }
+        else if (result) {
+          console.log('Connect success!');
+        }
+      }
+    );
   } catch (err) {
     throw err
-  } finally {
-    await client.close()
   }
-}
+};
 
 // Removes the user from the users array in the database.
 // If the user was the last user, the session is deleted from the database.
@@ -62,52 +103,127 @@ exports.disconnectUser = async function(roomId, userId) {
     await client.connect();
     const database = client.db("rtctele");
     const sessions = database.collection("sessions");
-    let filter = { room: roomId }
-    const result = await sessions.findOne(filter)
-    if (result != undefined) {
-      if (result.users.length > 1) {
-        let obj = { $pull: { users: userId } }
-        const update = await sessions.updateOne(filter, obj)
-        console.log(update)
-      } else {
-        const deleted = await sessions.deleteOne(filter)
-        console.log(deleted)
+    let filter = { room: roomId };
+    let operation = await sessions.findOne(
+      filter, (err, result) => {
+        if (err) {
+          throw err
+        }
+
+        else if (result) {
+          if (result.room == roomId) {
+            if (result.users.length > 1) {
+              remUser(roomId, userId);
+            } else {
+              delSession(roomId);
+            }
+          }
+        }
       }
-    }
+    );
   } catch (err) {
     throw err
-  } finally {
-    await client.close()
   }
-}
+};
 
-// Checks the status of the sessions
-exports.checkSession = async function(roomId, userId) {
+// Helper function for deleting a session.
+async function delSession(roomId) {
+  try {
+    await client.connect();
+    const database = client.db('rtctele');
+    const sessions = database.collection("sessions");
+    let filter = { room: roomId };
+    let operation = await sessions.deleteOne(
+      filter, (err, result) => {
+        if (err) {
+          throw err
+        }
+
+        else if (result) {
+          console.log("Deleted the session!");
+        }
+      }
+    );
+  } catch (err) {
+    throw err
+  }
+};
+
+// Helper function for removing a user.
+async function remUser(roomId, userId) {
   try {
     await client.connect();
     const database = client.db("rtctele");
     const sessions = database.collection("sessions");
-    let filter = { room: roomId }
-    const result = await sessions.find({ room: roomId })
-    if (result != undefined) {
-      if (result.users.length >= 2) {
-        let obj = { $set: { status: "Complete" } }
-        const update = await sessions.updateOne(filter, obj)
-        console.log(update)
-        console.log("Status updated:" + roomId)
-      } else if (result.users.length == 1) {
-        let obj = { $set: { status: "Started" } }
-        const update = await sessions.updateOne(filter, obj)
-        console.log(update)
-        console.log("Status updated:" + roomId)
+    let filter = { room: roomId };
+    let obj = { $pull: { users: userId } };
+    let operation = await sessions.updateOne(
+      filter, obj, (err, result) => {
+        if (err) {
+          throw err
+        }
+
+        else if (result) {
+          console.log("Disconnected a user!");
+        }
       }
-    }
+    );
+  } catch (err) {
+    throw err;
+  }
+};
+
+// Checks the status of the sessions
+exports.checkSession = async function(roomId, userId) {
+  try {
+    console.log('checking session...!')
+    await client.connect();
+    const database = client.db("rtctele");
+    const sessions = database.collection("sessions");
+    let filter = { room: roomId };
+    let operation = await sessions.findOne(
+      filter, (err, result) => {
+        if (err) {
+          throw err
+        }
+
+        else if (result) {
+          if (result.room == roomId) {
+            if (result.users.length >= 1) {
+              let statusType = "In Progress";
+              updateSession(roomId, statusType);
+            }
+          }
+        }
+      }
+    );
   } catch (err) {
     throw err
-  } finally {
-    await client.close()
   }
-}
+};
+
+async function updateSession(roomId, statusType) {
+  try {
+    await client.connect();
+    const database = client.db("rtctele");
+    const sessions = database.collection("sessions");
+    let filter = { room: roomId };
+    let obj = { $set: { status: statusType } };
+    let operation = await sessions.updateOne(
+      filter, obj, (err, result) => {
+        if (err) {
+          throw err
+        }
+
+        else if (result) {
+          console.log("Status updated to " + statusType + ": " + roomId);
+        }
+      }
+    );
+  } catch (err) {
+    throw err
+  }
+};
 
 //Returns a JSON object of all available rooms that are not complete.
 exports.getSessionList = async function(req, res) {
@@ -116,7 +232,26 @@ exports.getSessionList = async function(req, res) {
     const database = client.db("rtctele");
     const sessions = database.collection("sessions");
 
-    let filter = { status: 'New' }
+    var startDate = new Date();
+
+    startDate.setSeconds(0);
+    startDate.setHours(0);
+    startDate.setMinutes(0);
+
+    var dateMidnight = new Date(startDate);
+    dateMidnight.setHours(23);
+    dateMidnight.setMinutes(59);
+    dateMidnight.setSeconds(59);
+
+    let filter = {
+      status: {
+        $in: ["New"]
+      },
+      date : {
+        $gte: startDate,
+        $lte: dateMidnight
+      }
+    };
     let obj = {
       _id: 0,
       room: 1,
@@ -124,16 +259,19 @@ exports.getSessionList = async function(req, res) {
       users: 1,
       status: 1,
       date: 1
-    }
+    };
 
     const data = await sessions.find(filter, obj).toArray();
-    console.log(data)
+
     if (data.length == 0) {
-      console.log("No documents found.")
+      console.log("No documents found.");
     } else {
-      res.json(data)
-    }
+      res.json(data);
+    };
+
+  } catch (err) {
+    throw err
   } finally {
-    await client.close()
+    await client.close();
   }
-}
+};
